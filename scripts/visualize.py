fd2775259cc22c82c21c65f58093940d707ca45f
FantasticCowboy <lukehobeika@gmail.com>
2023-04-02 13:41:33 -0400

    updated visualize script - code is very bad but it gets the job done

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -8,6 +8,9 @@ from multiprocessing import Condition
 import time
 import math
 
+def printHelp():
+    print("Usage: visualize.py [Path to point cloud files] [numer of frames] [view single frame] [frame number if single frame]")
+
 DATA_SIZE = 4
 DATA_AMOUNT = 3
 STRUCT_SIZE = DATA_AMOUNT * DATA_SIZE
@@ -79,22 +82,13 @@ global pcd
 pcd = o3d.geometry.PointCloud()
 
 
-def main():
-    fileLocation_prefix = sys.argv[1]
-    numberOfFrames = int(sys.argv[2])
-    curFileNum = 0
-
-    global vis
+def playbackPointCloudMode():
+    global vis 
     global pcd
-    vis.create_window()
-    pcd.points = o3d.utility.Vector3dVector(readFile(getName(fileLocation_prefix, curFileNum)))
-    vis.add_geometry(pcd)
-    vis.poll_events()
-    vis.update_renderer()
     thread = Thread(target = controlPlayBack, args = (condition, False, 1))
     thread.start()
-
     i = 0
+    numberOfFrames = int(sys.argv[2])
     while i < numberOfFrames:
         vis.poll_events()
         vis.update_renderer()  
@@ -108,7 +102,33 @@ def main():
         i+=1
         condition.release()  
     
-    thread.join()
+    thread.join()    
+
+def main():
+
+    if(sys.argv[1] == "--help"):
+        printHelp()
+        return
+
+    fileLocation_prefix = sys.argv[1]
+    singleFrame = False
+    curFileNum = 0
+    if(len(sys.argv) > 3):
+        singleFrame = bool(sys.argv[3])
+        curFileNum = int(sys.argv[4])
+    global vis
+    global pcd
+    vis.create_window()
+    pcd.points = o3d.utility.Vector3dVector(readFile(getName(fileLocation_prefix, curFileNum)))
+    vis.add_geometry(pcd)
+    vis.poll_events()
+    vis.update_renderer()
+
+    if(not singleFrame):
+        playbackPointCloudMode()
+    else:
+        vis.run()
+
     vis.destroy_window()        
     
 
