11c219f44f80b244c6c30699c61a160dedea8ee0
FantasticCowboy <lukehobeika@gmail.com>
2023-04-25 16:58:32 +0000

    added in file transfer script

---
+++ scripts/getFile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+scp $1@68.181.32.203:/workspaces/FastDash/$2  $3
--- scripts/runFastDash.sh
+++ scripts/runFastDash.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-make FastDash && ./build/FastDash --frameHeight=200 --frameWidth=200 --calculateKeypoints=true --writeFramesToDisk=true 
+make FastDash && ./build/FastDash --frameHeight=200 --frameWidth=200 --calculateKeypoints=true --writeFramesToDisk=true  --transformFrames=true
